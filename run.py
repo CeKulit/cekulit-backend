876c68e412e447255595bf4203704a97685491e2
pangaribowo <fatahillahalifp04@gmail.com>
2025-03-13 15:47:20 +0700

    fix: issue

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from app.config import Config
 app = create_app()
 
 if __name__ == '__main__':
-    port = int(os.getenv('PORT', 5000))
+    port = int(os.getenv('PORT', 8080))
     app.run(host='0.0.0.0', port=port, debug=Config.FLASK_ENV == "development")
